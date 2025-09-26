f20e062c95e91a5bb84048e71afa92e845d06aff
gftmuthoni-cyber <gftmuthoni@gmail.com>
2025-09-26 11:28:44 +0300

    Add files via upload

---
+++ bank_balance.c
@@ -0,0 +1,15 @@
+/*
+Name :Gift Muthoni
+Reg No :CT101/G/26539/25
+Description :bank balance
+*/
+#include <stdio.h>
+
+int main() {
+    float bank_balance;
+    
+    printf("enter your bank_balance (in KSH)");
+    scanf("%f" ,&bank_balance);
+     printf(" your bank_balance is %.3f ");
+    return 0;
+}
